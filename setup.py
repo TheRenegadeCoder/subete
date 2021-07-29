6cae31c9f0639d550aa1ecb30d9378768d06c5a7
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-07-28 23:18:19 -0400

    Mimicked SnakeMD setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 import setuptools
 
+cmdclass = {}
+
+try:
+    from sphinx.setup_command import BuildDoc
+    cmdclass['build_sphinx'] = BuildDoc
+except ImportError:
+    print("WARNING: sphinx not available")
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+MAJOR = 0
+MINOR = 7
+PATCH = 0
+
+name = "subete"
+version = f"{MAJOR}.{MINOR}"
+release = f"{MAJOR}.{MINOR}.{PATCH}"
 setuptools.setup(
-    name="subete",
-    version="0.1.0",
+    name=name,
+    version=release,
     author="The Renegade Coder",
     author_email="jeremy.grifski@therenegadecoder.com",
     description="The Sample Programs API",
@@ -13,9 +28,24 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/TheRenegadeCoder/subete",
     packages=setuptools.find_packages(),
-    install_requires=[],
+    install_requires=[
+        "sphinx",
+        "sphinx_rtd_theme",
+        "sphinx-issues"
+    ],
     classifiers=(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ),
+    cmdclass=cmdclass,
+    command_options={
+        'build_sphinx': {
+            'project': ('setup.py', name),
+            'version': ('setup.py', version),
+            'release': ('setup.py', release),
+            'source_dir': ('setup.py', 'docsrc'),
+            'build_dir': ('setup.py', 'docsrc/_build'),
+            'builder': ("setup.py", "dirhtml")
+        }
+    }
 )
