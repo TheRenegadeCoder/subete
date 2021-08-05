759b53873f5f3bb05fd232fc7ba3f37f5f0ebbdb
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-05 19:30:47 -0400

    Added code testing

--- tests/test_sample_program.py
+++ tests/test_sample_program.py
@@ -7,3 +7,7 @@ TEST_LANG = "python"
 def test_sample_program_language():
     test = SampleProgram(TEST_FILE_PATH, TEST_FILE_NAME, TEST_LANG)
     assert test.language() == TEST_LANG
+
+def test_sample_program_code():
+    test = SampleProgram(TEST_FILE_PATH, TEST_FILE_NAME, TEST_LANG)
+    assert test.code() == 'print("Hello, World!")\n'
