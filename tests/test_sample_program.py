50513f279e54c4ef7870a132bfecf6e0c8f7eab1
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-05 19:20:42 -0400

    Updated test

--- tests/test_sample_program.py
+++ tests/test_sample_program.py
@@ -1,5 +1,8 @@
 from subete import SampleProgram
 
+TEST_FILE_NAME = "repo.py"
+TEST_FILE_PATH = "../subete/"
+
 def test_sample_program_language():
-    test = SampleProgram(None, None, "python")
+    test = SampleProgram(TEST_FILE_PATH, TEST_FILE_NAME, "python")
     assert test.language() == "python"
