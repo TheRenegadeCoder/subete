25c33127f49dd71f186ed215926921d85a7545fa
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-06 20:20:31 -0400

    Added testinfo url testing

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,3 +6,7 @@ TEST_REPO = subete.load()
 def test_doc_url_multiword_lang():
     language: LanguageCollection = TEST_REPO.language_collections()["Google Apps Script"]
     assert language.lang_docs_url() == "https://sample-programs.therenegadecoder.com/languages/google-apps-script"
+
+def test_testinfo_url_multiword_lang():
+    language: LanguageCollection = TEST_REPO.language_collections()["Google Apps Script"]
+    assert language.testinfo_url() == "https://github.com/TheRenegadeCoder/sample-programs/blob/main/archive/g/google-apps-script/testinfo.yml"
