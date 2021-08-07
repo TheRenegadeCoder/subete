28a4ccc948e044aec3f97870349f82c25b61a786
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-07 14:05:03 -0400

    Added some testing

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -7,11 +7,29 @@ def test_doc_url_multiword_lang():
     language: LanguageCollection = TEST_REPO.language_collections()["Google Apps Script"]
     assert language.lang_docs_url() == "https://sample-programs.therenegadecoder.com/languages/google-apps-script"
 
+def test_doc_url_symbol_lang():
+    language: LanguageCollection = TEST_REPO.language_collections()["C#"]
+    assert language.lang_docs_url() == "https://sample-programs.therenegadecoder.com/languages/c-sharp"
+
 def test_testinfo_url_multiword_lang():
     language: LanguageCollection = TEST_REPO.language_collections()["Google Apps Script"]
     assert language.testinfo_url() == "https://github.com/TheRenegadeCoder/sample-programs/blob/main/archive/g/google-apps-script/testinfo.yml"
+
+def test_testinfo_url_symbol_lang():
+    language: LanguageCollection = TEST_REPO.language_collections()["C#"]
+    assert language.testinfo_url() == "https://github.com/TheRenegadeCoder/sample-programs/blob/main/archive/c/c-sharp/testinfo.yml"
 
 def test_requirements_url_multiword_lang():
     language: LanguageCollection = TEST_REPO.language_collections()["Google Apps Script"]
     program: SampleProgram = language.sample_programs()["Hello World"]
     assert program.requirements_url() == "https://sample-programs.therenegadecoder.com/projects/hello-world"
+
+def test_documentation_url_multiword_lang():
+    language: LanguageCollection = TEST_REPO.language_collections()["Google Apps Script"]
+    program: SampleProgram = language.sample_programs()["Hello World"]
+    assert program.documentation_url() == "https://sample-programs.therenegadecoder.com/projects/hello-world/google-apps-script"
+
+def test_article_issue_url_multiword_lang():
+    language: LanguageCollection = TEST_REPO.language_collections()["Google Apps Script"]
+    program: SampleProgram = language.sample_programs()["Hello World"]
+    assert program.article_issue_query_url() == "https://github.com//TheRenegadeCoder/sample-programs-website/issues?utf8=%E2%9C%93&q=is%3Aissue+is%3Aopen+hello+world+google+apps+script"
