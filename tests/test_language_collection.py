1afd1d1a4bdb52d580d8d683a2cc9b4aab3b6c0a
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-04-29 12:03:24 -0400

    Fixed up tests a bit

--- tests/test_language_collection.py
+++ tests/test_language_collection.py
@@ -25,7 +25,7 @@ def test_language_collection_readme():
 def test_language_collection_sample_programs():
     test = subete.LanguageCollection(TEST_LANG, TEST_PATH, TEST_FILES, TEST_PROJECTS)
     assert test != None
-    assert subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG) in test
+    assert subete.SampleProgram(TEST_PATH, TEST_FILES[0], test) in test
 
 
 def test_language_collection_total_programs():
--- tests/test_sample_program.py
+++ tests/test_sample_program.py
@@ -17,42 +17,42 @@ TEST_LANG_COLLECTION: subete.LanguageCollection = subete.LanguageCollection(
 
 
 def test_sample_program_str():
-    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG)
+    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG_COLLECTION)
     assert str(test) == "Hello World in Python"
 
 
 def test_sample_program_language():
-    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG)
+    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG_COLLECTION)
     assert test.language_collection() == TEST_LANG
 
 
 def test_sample_program_code():
-    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG)
+    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG_COLLECTION)
     assert test.code() == 'print("Hello, World!")\n'
 
 
 def test_sample_program_line_count():
-    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG)
+    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG_COLLECTION)
     assert test.line_count() == 1
 
 
 def test_sample_program_size():
-    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG)
+    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG_COLLECTION)
     assert test.size() > 0
 
 
 def test_sample_program_requirements_url():
-    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG)
+    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG_COLLECTION)
     assert test.project().requirements_url(
     ) == "https://sampleprograms.io/projects/hello-world"
 
 
 def test_sample_program_documentation_url():
-    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG)
+    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG_COLLECTION)
     assert test.documentation_url() == "https://sampleprograms.io/projects/hello-world/python"
 
 
 def test_sample_program_issue_query_url():
-    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG)
+    test = subete.SampleProgram(TEST_PATH, TEST_FILES[0], TEST_LANG_COLLECTION)
     assert test.article_issue_query_url(
     ) == "https://github.com//TheRenegadeCoder/sample-programs-website/issues?utf8=%E2%9C%93&q=is%3Aissue+is%3Aopen+hello+world+python"
