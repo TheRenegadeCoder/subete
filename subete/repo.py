3689064ec3ff7bbd8643721dc168863b911cecda
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-05-09 22:38:07 -0400

    Added error log

--- subete/repo.py
+++ subete/repo.py
@@ -809,6 +809,7 @@ class SampleProgram:
         for project in projects:
             if project.pathlike_name() == url:
                 return project
+        logger.error(f"Could not find a project for {self._file_name} with name {url} in {projects}")
         return None
 
     def _generate_doc_url(self) -> str:
