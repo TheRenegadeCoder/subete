1808e70193519ed12fcbaf05815eff4e36a77238
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-05-16 11:38:33 -0400

    Reworked Project Naming (#52)

--- subete/repo.py
+++ subete/repo.py
@@ -910,7 +910,7 @@ class SampleProgram:
             url = stem.replace("_", "-").lower()
         else:
             # TODO: this is brutal. At some point, we should loop in the glotter test file.
-            url = re.sub('((?<=[a-z])[A-Z0-9]|(?!^)[A-Z](?=[a-z]))', r'-\1', stem).lower()
+            url = "-".join(re.sub('([A-Z][a-z]+)', r' \1', re.sub('([A-Z]+)', r' \1', stem)).split()).lower()
         logger.info(f"Constructed a normalized form of the program {url}")
         for project in projects:
             if url in project.pathlike_name():
