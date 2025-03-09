b906c32b676d634c8764a4a6c6555c78a29d6e3a
rzuckerm <ron.zuckerman@adtran.com>
2025-03-09 16:42:40 -0500

    Forgot to update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setuptools.setup(
     ],
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
